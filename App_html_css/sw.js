99d19a1e77f3d63d8f82b2ad09c07495163f2cfa
KeishiYamamoto-a-zs <135563899+KeishiYamamoto-a-zs@users.noreply.github.com>
2023-08-08 15:22:59 +0900

    Add files via upload

---
+++ App_html_css/sw.js
@@ -0,0 +1,47 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './css/bootstrap.css',
+  './css/inputform.css',
+  './css/question.css',
+  './css/result1.css',
+  './css/title.css',
+  './images/a-zs_logo.png',
+  './images/animalCrossing.jpg',
+  './images/apex.jpg',
+  './images/valorant.png',
+  './app.js',
+  './demoResult.html',
+  './demoResult2.html',
+  './demoResult3.html',
+  './informationConsent.html',
+  './inputform.html',
+  './question.html',
+  './title.html',
+];
+
+//swは正常に登録されるとinstallイベントが走る
+//正常に登録されたら、キャッシュにurlsToCacheに入れたファイルを次々とキャッシュしていく（All-or-Nothing特性）
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+//ページ遷移のときに、リクエストされたファイルがキャッシュの中にあれば該当キャッシュファイルを返す
+//なかった場合はネットワークから取得しようとする
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
